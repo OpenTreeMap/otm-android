0424baf003710e5e03106015b270ed90fe9a772a
Matthew McFarland <mmcfarland@beijing.internal.azavea.com>
2012-07-27 14:26:44 -0400

    Add Authenticated version of GET, POST, DELETE
    Implemented the async client methods which allow adding headers
    in order to provide requests with basic authentication.  The caller
    will have to pass in username/password, so it is assumed that API layer
    will know which calls need auth, and have that integrated into their
    signatures.

--- OpenTreeMap/src/org/azavea/otm/rest/RestClient.java
+++ OpenTreeMap/src/org/azavea/otm/rest/RestClient.java
@@ -1,5 +1,10 @@
 package org.azavea.otm.rest;
 
+import org.apache.http.Header;
+import org.apache.http.message.BasicHeader;
+
+import android.content.Context;
+import android.util.Base64;
 import android.util.Log;
 
 import com.loopj.android.http.*;
@@ -31,13 +36,44 @@ public class RestClient {
 		client.get(getAbsoluteUrl(url), reqParams, responseHandler);
 	}
 	
+	/**
+	 * Executes a get request and adds basic authentication headers to the request.
+	 */
+	public void getWithAuthentication(Context context, String url, String username, 
+			String password, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+		RequestParams reqParams = prepareParams(params);
+		Header[] headers = {createBasicAuthenticationHeader(username, password)};
+		client.get(context, getAbsoluteUrl(url), headers, reqParams, responseHandler);
+	}
+	
 	public void post(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
 		RequestParams reqParams = prepareParams(params);
 		client.post(getAbsoluteUrl(url), reqParams, responseHandler);
 	}
 	
+	/**
+	 * Executes a post request and adds basic authentication headers to the request.
+	 */
+	public void postWithAuthentication(Context context, String url, String username, 
+			String password, RequestParams params, String contentType, 
+			AsyncHttpResponseHandler responseHandler) {
+		RequestParams reqParams = prepareParams(params);
+		Header[] headers = {createBasicAuthenticationHeader(username, password)};
+		client.post(context, getAbsoluteUrl(url), headers, reqParams, contentType, 
+				responseHandler);
+	}
+
 	public void delete(String url, AsyncHttpResponseHandler responseHandler) {
 		client.delete(getAbsoluteUrl(url), responseHandler);
+	}
+
+	/**
+	 * Executes a delete request and adds basic authentication headers to the request.
+	 */
+	public void deleteWithAuthentication(Context context, String url, String username, 
+			String password, AsyncHttpResponseHandler responseHandler) {
+		Header[] headers = {createBasicAuthenticationHeader(username, password)};
+		client.delete(context, getAbsoluteUrl(url), headers, responseHandler);
 	}
 	
 	private RequestParams prepareParams(RequestParams params) {
@@ -68,5 +104,11 @@ public class RestClient {
 	
 	private String getAbsoluteUrl(String relativeUrl) {
 		return baseUrl + relativeUrl;
+	}
+	
+	private Header createBasicAuthenticationHeader(String username, String password) {
+		String credentials = String.format("%s:%s", username, password);
+		String encoded = Base64.encodeToString(credentials.getBytes(), Base64.DEFAULT);
+		return new BasicHeader("Authorization", String.format("%s %s", "Basic", encoded));
 	}
 }
