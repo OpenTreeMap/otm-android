51ccb848a6d9e4bba52f07e1c95a3deab5200e77
Matthew McFarland <mmcfarland@azavea.com>
2012-07-31 12:56:42 -0400

    Add login manager to request generator
    All login credentials will be provided by the login manager

--- OpenTreeMap/src/org/azavea/otm/rest/RequestGenerator.java
+++ OpenTreeMap/src/org/azavea/otm/rest/RequestGenerator.java
@@ -2,6 +2,8 @@ package org.azavea.otm.rest;
 
 import java.io.UnsupportedEncodingException;
 
+import org.azavea.otm.App;
+import org.azavea.otm.LoginManager;
 import org.azavea.otm.data.Plot;
 import org.azavea.otm.data.User;
 import org.json.JSONException;
@@ -15,6 +17,7 @@ import com.loopj.android.http.JsonHttpResponseHandler;
 
 public class RequestGenerator {
 	private RestClient client;
+	LoginManager loginManager = App.getLoginManager();
 	
 	public RequestGenerator() {
 		client = new RestClient();
@@ -33,15 +36,39 @@ public class RequestGenerator {
 		client.get("/plots/" + id, null, handler);
 	}
 	
-	public void updatePlot(Context context, int id, Plot plot, AsyncHttpResponseHandler handler) throws UnsupportedEncodingException {
-		client.putWithAuthentication(context, "/plots/", "administrator", "123456", id, plot, handler);
+	public void updatePlot(Context context, int id, Plot plot, 
+			AsyncHttpResponseHandler handler) throws UnsupportedEncodingException {
+		if (loginManager.isLoggedIn()) {
+			try {
+				client.putWithAuthentication(context, "/plots/", 
+						loginManager.loggedInUser.getUserName(), 
+						loginManager.loggedInUser.getPassword(), id, plot, handler);
+			} catch (JSONException e) {
+				handleBadResponse(e);
+			}
+		} else {
+			redirectToLoginActivity();
+		}
+	}
+
+	public void addUser(Context context, User user, AsyncHttpResponseHandler handler) 
+			throws JSONException, UnsupportedEncodingException {
+		client.postWithAuthentication(context, "/user/", 
+				loginManager.loggedInUser.getUserName(), 
+				loginManager.loggedInUser.getPassword(), user, handler);
 	}
 	
-	public void addUser(Context context, User user, AsyncHttpResponseHandler handler) throws JSONException, UnsupportedEncodingException {
-		client.postWithAuthentication(context, "/user/", "administrator", "123456", user, handler);
-	}
-	
-	public void logIn(Context context, String username, String password, JsonHttpResponseHandler handler) {
+	public void logIn(Context context, String username, String password, 
+			JsonHttpResponseHandler handler) {
 		client.getWithAuthentication(context, "/login", username, password, null, handler);
+	}
+	
+	private void handleBadResponse(JSONException e) {
+		Log.e(App.LOG_TAG, "Unable to parse JSON response", e);
+	}	
+	
+	private void redirectToLoginActivity() {
+		// TODO Auto-generated method stub
+		
 	}
 }
