a820221930cbe3f98a4723339f76ffc16ded8cf4
Matthew McFarland <mmcfarland@azavea.com>
2012-07-31 12:55:31 -0400

    Add helper method to handle null long values
    The json may represent as NULL, which is not valid for a long value.
    This method will provide a default value if it is null in json.

--- OpenTreeMap/src/org/azavea/otm/data/Model.java
+++ OpenTreeMap/src/org/azavea/otm/data/Model.java
@@ -50,6 +50,13 @@ public abstract class Model {
 //		}
 //	}
 	
+	protected long getLongOrDefault(String key, Long defaultValue) throws JSONException {
+		if (data.isNull(key)){ 
+			return defaultValue;
+		} else {
+			return data.getLong(key);
+		}
+	}
 	public void setData(JSONObject data) {
 		this.data = data;
 	}
