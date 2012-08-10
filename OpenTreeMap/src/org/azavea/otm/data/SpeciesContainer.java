3fc0dafc2e6d4b43b72745042e54929402fee0ea
Matthew McFarland <mmcfarland@azavea.com>
2012-08-10 14:30:52 -0400

    Implement getAll to return Hash
    Species come back ordered by common name.  Preserving this sorting lets
    us change the default sort order by just changing the API, which we have
    more control over than for installed applications.

--- OpenTreeMap/src/org/azavea/otm/data/SpeciesContainer.java
+++ OpenTreeMap/src/org/azavea/otm/data/SpeciesContainer.java
@@ -1,21 +1,26 @@
 package org.azavea.otm.data;
 
+import java.util.LinkedHashMap;
+
 import org.json.JSONException;
 
 public class SpeciesContainer extends ModelContainer<Species> {
 
 	@Override
-	public Species[] getAll() {
-		Species[] species = new Species[data.length()];
+	public LinkedHashMap<Integer,Species> getAll() {
+		LinkedHashMap<Integer,Species> speciesList = 
+				new LinkedHashMap<Integer,Species>(data.length());
 		for (int i = 0; i < data.length(); i++) {
-			species[i] = new Species();
 			try {
-				species[i].setData(data.getJSONObject(i));
+				Species species = new Species();
+				species.setData(data.getJSONObject(i));
+				speciesList.put(species.getId(), species);
+				
 			} catch (JSONException e) {
 				e.printStackTrace();
 			}
 		}
-		return species;
+		return speciesList;
 	}
 
 }
