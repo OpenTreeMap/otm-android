b89cdc3dc273a249f84d6455f2064331e3bb81b9
Matthew McFarland <mmcfarland@azavea.com>
2012-08-15 18:27:48 -0400

    Remove unused imports

--- OpenTreeMap/src/org/azavea/otm/ui/MapDisplay.java
+++ OpenTreeMap/src/org/azavea/otm/ui/MapDisplay.java
@@ -1,12 +1,10 @@
 package org.azavea.otm.ui;
 
-import java.io.Serializable;
 import java.util.List;
 
 import org.azavea.otm.App;
 import org.azavea.otm.R;
 import org.azavea.otm.data.Plot;
-import org.azavea.otm.data.User;
 import org.azavea.otm.rest.RequestGenerator;
 import org.azavea.otm.rest.handlers.RestHandler;
 import org.json.JSONException;
@@ -25,7 +23,6 @@ import com.google.android.maps.MapController;
 import com.google.android.maps.MapView;
 import com.google.android.maps.MyLocationOverlay;
 import com.google.android.maps.Overlay;
-import com.loopj.android.http.JsonHttpResponseHandler;
 
 
 public class MapDisplay extends MapActivity {
