38ea6b120f2c0bdd8013e47ea0e6b4affa8b6d9c
Matthew Delaney <mdelaney@azavea.com>
2012-09-12 17:15:22 -0400

    Switched to mockito and modified manifest to support tests.
    The annotation-processor used by AndroidMock was
    preventing ant-based building of and running of
    the unit-tests so I've switched over to mockito.
    
    I've also made Google Maps optional for the application
    as it isn't necessary for the tests to run and requiring
    it prevents deployment to androidx86 in VirtualBox since
    that image doesn't have the Google Maps library.

--- OpenTreeMapTests/src/org/azavea/otm/test/RequestGeneratorTest.java
+++ OpenTreeMapTests/src/org/azavea/otm/test/RequestGeneratorTest.java
@@ -5,9 +5,10 @@ import org.azavea.otm.rest.RequestGenerator;
 import org.azavea.otm.rest.RestClient;
 import org.azavea.otm.rest.handlers.RestHandler;
 
-import com.google.android.testing.mocking.AndroidMock;
-import com.google.android.testing.mocking.UsesMocks;
+//import com.google.android.testing.mocking.AndroidMock;
+//import com.google.android.testing.mocking.UsesMocks;
 import com.loopj.android.http.JsonHttpResponseHandler;
+import static org.mockito.Mockito.*;
 
 import junit.framework.TestCase;
 
@@ -23,28 +24,21 @@ public class RequestGeneratorTest extends TestCase {
 		super.tearDown();
 	}
 	
-	@UsesMocks(RestClient.class)
 	public void testGetVersion() {
 		// Create a handler
 		RestHandler<Version> handler = new RestHandler<Version>(new Version());
-		
 		// Create a mock RestClient
-		RestClient mockClient = AndroidMock.createMock(RestClient.class);
-		
-		// Specify required call conditions
-		mockClient.get("/version", null, handler);
-		
-		AndroidMock.replay(mockClient);
-		
+		RestClient mockClient = mock(RestClient.class);
+
 		// Inject mock RestClient into RequestGenerator
 		rg.setClient(mockClient);
-		
+
 		// Make the call
 		rg.getVersion(handler);
-		
+
 		// See if mock RestClient was called
 		// correctly
-		AndroidMock.verify(mockClient);
+		verify(mockClient).get("/version", null, handler);
 	}
 		
 }
