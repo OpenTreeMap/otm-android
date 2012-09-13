9f072f684056cdc8835d9d59fc2e126c58abc996
Matthew Delaney <mdelaney@azavea.com>
2012-09-13 11:41:15 -0400

    Switched unit-tests to Mockito and removed AndroidMock libraries.

--- OpenTreeMapTests/src/org/azavea/otm/test/RestClientTest.java
+++ OpenTreeMapTests/src/org/azavea/otm/test/RestClientTest.java
@@ -10,8 +10,6 @@ import org.json.JSONObject;
 
 import android.util.Log;
 
-//import com.google.android.testing.mocking.AndroidMock;
-//import com.google.android.testing.mocking.UsesMocks;
 import com.loopj.android.http.*;
 
 import junit.framework.Assert;
@@ -19,13 +17,15 @@ import junit.framework.TestCase;
 
 import java.lang.Object;
 
+import static org.mockito.Mockito.*;
+
 public class RestClientTest extends TestCase {
 	private RestClient rc;
 	private String urlBase;
 	
 	public void setUp() {
 		rc = new RestClient();
-		urlBase = "http://10.0.2.2:8000/api/v0.1";
+		urlBase = "http://207.245.89.246/v1.2/api/v0.1";
 	}
 	
 	public void tearDown() {
@@ -35,29 +35,24 @@ public class RestClientTest extends TestCase {
 	
 	// Does .get() correctly process its parameters
 	// and route them to the underlying AsyncHttpClient object?
-//	@UsesMocks(AsyncHttpClient.class)
 	public void testGet() throws ClassNotFoundException {
 		// All we care about as far as these are concerned is that
 		// the objects we pass to the RestClient object and passed to the
 		// underlying AsyncHttpClient object
-//		RequestParams params = new RequestParams();
-//		JsonHttpResponseHandler jsonClient = new JsonHttpResponseHandler();
-//		
-//		// Create a mock async client that expects a complete URL
-//		AsyncHttpClient mockClient = AndroidMock.createMock(AsyncHttpClient.class);
-//		mockClient.get(urlBase + "/get", params, jsonClient);
-//		
-//		// Begin test-sequence
-//		AndroidMock.replay(mockClient);
-//
-//		// Inject mock
-//		rc.setAsyncClient(mockClient);
-//
-//		// Make the call
-//		rc.get("/get", params, jsonClient);
-//		
-//		// See if mockClient.get() got called as expected
-//		AndroidMock.verify(mockClient);
+		RequestParams params = new RequestParams();
+		JsonHttpResponseHandler jsonClient = new JsonHttpResponseHandler();
+		
+		// Create a mock async client that expects a complete URL
+		AsyncHttpClient mockClient = mock(AsyncHttpClient.class);
+
+		// Inject mock
+		rc.setAsyncClient(mockClient);
+
+		// Make the call
+		rc.get("/get", params, jsonClient);
+		
+		// See if mockClient.get() got called as expected
+		verify(mockClient).get(urlBase + "/get", params, jsonClient);
 	}
 	
 	// Does .post() correctly process its parameters
@@ -67,68 +62,54 @@ public class RestClientTest extends TestCase {
 		// All we care about as far as these are concerned is that
 		// the objects we pass to the RestClient object and passed to the
 		// underlying AsyncHttpClient object
-//		RequestParams params = new RequestParams();
-//		JsonHttpResponseHandler jsonClient = new JsonHttpResponseHandler();
-//		
-//		// Create a mock async client that expects a complete URL
-//		AsyncHttpClient mockClient = AndroidMock.createMock(AsyncHttpClient.class);
-//		mockClient.post(urlBase + "/put", params, jsonClient);
-//		
-//		// Begin test-sequence
-//		AndroidMock.replay(mockClient);
-//
-//		// Inject mock
-//		rc.setAsyncClient(mockClient);
-//
-//		// Make the call
-//		rc.post("/put", params, jsonClient);
-//		
-//		// See if mockClient.get() got called as expected
-//		AndroidMock.verify(mockClient);
+		RequestParams params = new RequestParams();
+		JsonHttpResponseHandler jsonClient = new JsonHttpResponseHandler();
+		
+		// Create a mock async client that expects a complete URL
+		AsyncHttpClient mockClient = mock(AsyncHttpClient.class);
+
+		// Inject mock
+		rc.setAsyncClient(mockClient);
+
+		// Make the call
+		rc.post("/put", params, jsonClient);
+		
+		// See if mockClient.get() got called as expected
+		verify(mockClient).post(urlBase + "/put",  params, jsonClient);
 	}
 	
 	// Does .delete() correctly process its parameters
 	// and route them to the underlying AsyncHttpClient object?
-//	@UsesMocks(AsyncHttpClient.class)
 	public void testDelete() throws ClassNotFoundException {
 		// All we care about as far as these are concerned is that
 		// the objects we pass to the RestClient object and passed to the
 		// underlying AsyncHttpClient object
-//		JsonHttpResponseHandler jsonClient = new JsonHttpResponseHandler();
-//		
-//		// Create a mock async client that expects a complete URL
-//		AsyncHttpClient mockClient = AndroidMock.createMock(AsyncHttpClient.class);
-//		mockClient.delete(urlBase + "/delete", jsonClient);
-//		
-//		// Begin test-sequence
-//		AndroidMock.replay(mockClient);
-//
-//		// Inject mock
-//		rc.setAsyncClient(mockClient);
-//
-//		// Make the call
-//		rc.delete("/delete", jsonClient);
-//		
-//		// See if mockClient.get() got called as expected
-//		AndroidMock.verify(mockClient);
+		JsonHttpResponseHandler jsonClient = new JsonHttpResponseHandler();
+		
+		// Create a mock async client that expects a complete URL
+		AsyncHttpClient mockClient = mock(AsyncHttpClient.class);
+
+		// Inject mock
+		rc.setAsyncClient(mockClient);
+
+		// Make the call
+		rc.delete("/delete", jsonClient);
+		
+		// See if mockClient.delete() got called as expected
+		verify(mockClient).delete(urlBase + "/delete", jsonClient);
 	}
 	
 	// Is a null parameter-value for RequestParams
 	// replaced with a RequestParams object?
-//	@UsesMocks(AsyncHttpClient.class)
 	public void testNullParams() throws ClassNotFoundException {
-//		AsyncHttpClient mockClient = AndroidMock.createMock(AsyncHttpClient.class);
-//		
-//		JsonHttpResponseHandler jsonClient = new JsonHttpResponseHandler();
-//		mockClient.get(AndroidMock.eq(urlBase + "/get"), (RequestParams)AndroidMock.anyObject(), AndroidMock.eq(jsonClient));
-//		
-//		AndroidMock.replay(mockClient);
-//		
-//		rc.setAsyncClient(mockClient);
-//		
-//		Log.d("mock", "making call for rc.get");
-//		rc.get("/get", null, jsonClient);
-//		
-//		AndroidMock.verify(mockClient);
+		AsyncHttpClient mockClient = mock(AsyncHttpClient.class);
+		
+		JsonHttpResponseHandler jsonClient = new JsonHttpResponseHandler();
+		
+		rc.setAsyncClient(mockClient);
+		
+		rc.get("/get", null, jsonClient);
+		
+		verify(mockClient).get(eq(urlBase + "/get"), isA(RequestParams.class), eq(jsonClient));
 	}
 }
