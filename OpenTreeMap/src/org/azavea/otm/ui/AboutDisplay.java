158d4fb119ff33901630463ffa177314f063e854
Sam Halperin <sqh@me.com>
2012-12-05 17:59:53 -0500

    about page webview

--- OpenTreeMap/src/org/azavea/otm/ui/AboutDisplay.java
+++ OpenTreeMap/src/org/azavea/otm/ui/AboutDisplay.java
@@ -4,11 +4,15 @@ import org.azavea.otm.R;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class AboutDisplay extends Activity{
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.about_activity);
+        WebView wv = (WebView) findViewById(R.id.about_webview);
+        wv.loadUrl("file:///android_asset/about_content.html");
+        
     }
 }
