8c1a387805392310565cd62a68746dca4db120a9
Sam Halperin <sqh@me.com>
2013-02-06 17:09:03 -0500

    stubbed out code to present a main tab bar highlighted icon

--- OpenTreeMap/src/org/azavea/otm/ui/TabLayout.java
+++ OpenTreeMap/src/org/azavea/otm/ui/TabLayout.java
@@ -21,25 +21,25 @@ public class TabLayout extends TabActivity {
         // Map
         Intent intent = new Intent().setClass(this, MainMapActivity.class);
         spec = tabHost.newTabSpec("map").setIndicator("Tree Map", 
-        		res.getDrawable(R.drawable.ic_treemarker)).setContent(intent);
+        		res.getDrawable(R.drawable.tab_tree_map)).setContent(intent);
         tabHost.addTab(spec);
 
         // Profile
         intent = new Intent().setClass(this, ProfileDisplay.class);
         spec = tabHost.newTabSpec("profile").setIndicator("Profile", 
-        		res.getDrawable(R.drawable.ic_profile)).setContent(intent);
+        		res.getDrawable(R.drawable.tab_profile)).setContent(intent);
         tabHost.addTab(spec);
         
         // List
         intent = new Intent().setClass(this, ListDisplay.class);
         spec = tabHost.newTabSpec("list").setIndicator("Lists", 
-        		res.getDrawable(R.drawable.ic_list)).setContent(intent);
+        		res.getDrawable(R.drawable.tab_list)).setContent(intent);
         tabHost.addTab(spec);
         
         // About
         intent = new Intent().setClass(this, AboutDisplay.class);
         spec = tabHost.newTabSpec("about").setIndicator("About", 
-        		res.getDrawable(R.drawable.ic_about)).setContent(intent);
+        		res.getDrawable(R.drawable.tab_about)).setContent(intent);
         tabHost.addTab(spec);
 
         tabHost.setCurrentTab(3);
