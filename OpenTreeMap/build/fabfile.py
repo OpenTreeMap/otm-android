1d31feccaeb739312adef4d88da8403865668300
Sam Halperin <sqh@me.com>
2013-02-08 13:46:42 -0500

    drawables working

--- OpenTreeMap/build/fabfile.py
+++ OpenTreeMap/build/fabfile.py
@@ -1,5 +1,7 @@
 from fabric.api import local
 import json
+import shutil
+import os
 
 def _get_json(path):
 	json_data=open(path)
@@ -52,20 +54,43 @@ def strings():
 	_write_out(template_path, json_path, out_path)
 
 def drawables():
-	#TODO clean targets first??
-
 	# TARGET drawable directories
 	hdpi_tgt   = buildconf["project"] + "/res/drawable-hdpi"
 	ldpi_tgt   = buildconf["project"] + "/res/drawable-ldpi"
 	mdpi_tgt   = buildconf["project"] + "/res/drawable-mdpi"	
-	xhdpi_tgt  = buildconf["project"] + "/res/drawable-xdpi"
+	xhdpi_tgt  = buildconf["project"] + "/res/drawable-xhdpi"
 
+	# start from a clean dir.  I decided this was preferable to having
+	# stuff hanging around the eclipse project that is not going to be part
+	# of a fresh build.
+	try:
+		shutil.rmtree(hdpi_tgt)
+	except:
+		pass
+	try:
+		shutil.rmtree(ldpi_tgt)
+	except:
+		pass
+	try:
+		shutil.rmtree(mdpi_tgt)
+	except:
+		pass
+	try:
+		shutil.rmtree(xhdpi_tgt)
+	except:
+		pass
+
+	os.makedirs(hdpi_tgt)
+	os.makedirs(ldpi_tgt)
+	os.makedirs(mdpi_tgt)
+	os.makedirs(xhdpi_tgt)
 
 	# FIRST copy all files from default
 	hdpi_src   = buildconf["default"] + "/drawable-hdpi/*"
 	ldpi_src   = buildconf["default"] + "/drawable-ldpi/*"
 	mdpi_src   = buildconf["default"] + "/drawable-mdpi/*"
 	xhdpi_src  = buildconf["default"] + "/drawable-xhdpi/*"
+	
 	local("cp " + hdpi_src + " " + hdpi_tgt)
 	local("cp " + ldpi_src + " " + ldpi_tgt)
 	local("cp " + mdpi_src + " " + mdpi_tgt)
