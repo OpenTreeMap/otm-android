88e676554db8adb3d9500368e29652742a12e561
Matthew McFarland <mmcfarland@azavea.com>
2014-03-25 17:00:49 -0400

    Auto-format syntax
    Ran the eclipse auto format to remove tabs and give proper
    line endings

--- OpenTreeMap/src/org/azavea/otm/rest/RestClient.java
+++ OpenTreeMap/src/org/azavea/otm/rest/RestClient.java
@@ -25,245 +25,264 @@ import com.loopj.android.http.RequestParams;
 // This class is designed to take care of the base-url
 // and otm api-key for REST requests
 public class RestClient {
-	private String baseUrl;
-	
-	private String apiKey;
-	
-	private AsyncHttpClient client;
-	
-	private SharedPreferences prefs;
-	
-	private String appVersion;
-	
-	public RestClient() {
-		prefs = App.getSharedPreferences();
-		baseUrl = getBaseUrl();
-		apiKey = getApiKey();
-		appVersion = getAppVersion();
-		client = createHttpClient();
-	}
+    private String baseUrl;
 
-	// Dependency injection to support mocking
-	// in unit-tests
-	public void setAsyncClient(AsyncHttpClient client) {
-		this.client = client;
-	}
-	
-	public void cancelRequests(Context context) {
-		client.cancelRequests(context, true);
-	}
-	
-	public void get(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
-		RequestParams reqParams = prepareParams(params);
-		Log.d("rc", baseUrl);
-		client.get(getAbsoluteUrl(url), reqParams, responseHandler);
-	}
-	
-	public void post(Context context, String url, int id, Model model, AsyncHttpResponseHandler response) throws UnsupportedEncodingException {
-		String completeUrl = getAbsoluteUrl(url) + id;
-		completeUrl = prepareUrl(completeUrl);
-		client.post(context, completeUrl, new StringEntity(model.getData().toString()), "application/json", response);
-	}
+    private String apiKey;
 
-	public void post(Context context, String url, Model model, AsyncHttpResponseHandler response) throws UnsupportedEncodingException {
-		String completeUrl = getAbsoluteUrl(url);
-		completeUrl = prepareUrl(completeUrl);
-		client.post(context, completeUrl, new StringEntity(model.getData().toString()), "application/json", response);
-	}
+    private AsyncHttpClient client;
 
-	
-	public void post(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
-		RequestParams reqParams = prepareParams(params);
-		client.post(getAbsoluteUrl(url), reqParams, responseHandler);
-	}
-	
-	
-	
-	public void put(Context context, String url, int id, Model model, 
-			AsyncHttpResponseHandler response) throws UnsupportedEncodingException {
-		String completeUrl = getAbsoluteUrl(url) + id;
-		completeUrl = prepareUrl(completeUrl);
-		client.put(context, completeUrl, new StringEntity(model.getData().toString()), "application/json", response);
-	}
-	
-	/**
-	 * Executes a get request and adds basic authentication headers to the request.
-	 */
-	public void getWithAuthentication(Context context, String url, String username, 
-		String password, RequestParams params, AsyncHttpResponseHandler responseHandler) {
-		RequestParams reqParams = prepareParams(params);
-		Header[] headers = {createBasicAuthenticationHeader(username, password)};
-		client.get(context, getAbsoluteUrl(url), headers, reqParams, responseHandler);
-	}
+    private SharedPreferences prefs;
 
-	/**
-	 * Executes a put request and adds basic authentication headers to the request.
-	 */
-	public void putWithAuthentication(Context context,
-									  String url,
-									  String username,
-									  String password,
-									  int id,
-									  Model model,
-									  AsyncHttpResponseHandler response) throws UnsupportedEncodingException {
-		
-		String completeUrl = getAbsoluteUrl(url) + id;
-		completeUrl = prepareUrl(completeUrl);
-		Header[] headers = {createBasicAuthenticationHeader(username, password)};
-		StringEntity modelEntity = new StringEntity(model.getData().toString());
-		Log.d("REST", model.getData().toString());
-		client.put(context, completeUrl, headers, modelEntity, "application/json", response);
-	}
-	
-	public void putWithAuthentication(Context context,
-			  String url,
-			  String username,
-			  String password,
-			  Model model,
-			  AsyncHttpResponseHandler response) throws UnsupportedEncodingException {
+    private String appVersion;
 
-		String completeUrl = getAbsoluteUrl(url);
-		completeUrl = prepareUrl(completeUrl);
-		Header[] headers = {createBasicAuthenticationHeader(username, password)};
-		StringEntity modelEntity = new StringEntity(model.getData().toString());
-		
-		client.put(context, completeUrl, headers, modelEntity, "application/json", response);
-	}
+    public RestClient() {
+        prefs = App.getSharedPreferences();
+        baseUrl = getBaseUrl();
+        apiKey = getApiKey();
+        appVersion = getAppVersion();
+        client = createHttpClient();
+    }
 
-	/**
-	 * Executes a post request and adds basic authentication headers to the request.
-	 */
-	public void postWithAuthentication(Context context,
-									   String url,
-									   String username, 
-									   String password,
-									   Model model, 
-									   AsyncHttpResponseHandler responseHandler) throws UnsupportedEncodingException {
-		
-		String completeUrl = getAbsoluteUrl(url);
-		completeUrl = prepareUrl(completeUrl);
-		Header[] headers = {createBasicAuthenticationHeader(username, password)};
-		StringEntity modelEntity = new StringEntity(model.getData().toString());
-		
-		Log.d("REST", model.getData().toString());
-		client.post(context, completeUrl, headers, modelEntity, "application/json", 
-				responseHandler);
-	}	
-	
-	public void postWithAuthentication(Context context,
-			   String url,
-			   String username, 
-			   String password,
-			   AsyncHttpResponseHandler responseHandler) throws UnsupportedEncodingException {
+    // Dependency injection to support mocking
+    // in unit-tests
+    public void setAsyncClient(AsyncHttpClient client) {
+        this.client = client;
+    }
 
-		String completeUrl = getAbsoluteUrl(url);
-		completeUrl = prepareUrl(completeUrl);
-		Header[] headers = {createBasicAuthenticationHeader(username, password)};
-		StringEntity modelEntity = new StringEntity("");
-		client.post(context, completeUrl, headers, modelEntity, "application/json", 
-		responseHandler);
-}	
-	
-	
-	// This overloading of the postWithAuthentication method takes a bitmap, and posts
-	// it as an PNG HTTP Entity.
-	public void postWithAuthentication(Context context, String url, Bitmap bm, String username,
-			String password, JsonHttpResponseHandler responseHandler, int timeout) {
-		
-		String completeUrl = getAbsoluteUrl(url);
-		completeUrl = prepareUrl(completeUrl);
-		
-		// Content type also needs to be pinned down in the Bitmap.compress
-		// call, which is why I haven't exposed it as a parameter.
-		String contentType = "image/jpeg";
-				
-		// We need to coerce the bitmap into a ByteArrayEntity so that we can post it.
-		ByteArrayOutputStream bos = new ByteArrayOutputStream(); 
-		bm.compress(CompressFormat.JPEG, 55, bos); 
-		byte[] bitmapdata = bos.toByteArray();
-		ByteArrayEntity bae = new ByteArrayEntity(bitmapdata);
-		
-		// This client needs authentication headers because there is no post method that takes both
-		// an entity and headers.  I assert that we don't get anything for free anyway by having one
-		// global AsyncHttpClient object because instantiation of that class is so easy.
-		AsyncHttpClient authenticatedClient = createAutheniticatedHttpClient(username, password);
-		
-		authenticatedClient.setTimeout(timeout);
-		authenticatedClient.post(context, completeUrl, bae, contentType, responseHandler);
-	}
-	
-	public void delete(String url, AsyncHttpResponseHandler responseHandler) {
-		client.delete(getAbsoluteUrl(url), responseHandler);
-	}
+    public void cancelRequests(Context context) {
+        client.cancelRequests(context, true);
+    }
 
-	/**
-	 * Executes a delete request and adds basic authentication headers to the request.
-	 */
-	public void deleteWithAuthentication(Context context, String url, String username, 
-			String password, AsyncHttpResponseHandler responseHandler) {
-		String completeUrl = getAbsoluteUrl(url);
-		completeUrl = prepareUrl(completeUrl);
-		Header[] headers = {createBasicAuthenticationHeader(username, password)};
-		client.delete(context, completeUrl, headers, responseHandler);
-	}
-	
-	private RequestParams prepareParams(RequestParams params) {
-		// We'll always need a RequestParams object since we'll always
-		// be sending an apikey
-		RequestParams reqParams;
-		if (params == null) {
-			reqParams = new RequestParams();
-		} else {
-			reqParams = params;
-		}
-		
-		reqParams.put("apikey", apiKey);
-		
-		return reqParams;
-	}
-	
-	private String prepareUrl(String url) {
-		// Not all methods of AsynchHttpClients take a requestParams.
-		// Sometimes we will need to put the api key and other data
-		// directly in the URL.
-		return url + "?apikey=" + getApiKey(); 
-	}
-	
-	private String getBaseUrl() {
-		String baseUrl = prefs.getString("base_url", "");
-		return baseUrl;
-	}
-	
-	private String getApiKey() {
-		String apiKey = prefs.getString("api_key", "");
-		return apiKey;
-	}
-	
-	private String getAbsoluteUrl(String relativeUrl) {
-		Log.d(App.LOG_TAG, baseUrl + relativeUrl);
-		return baseUrl + relativeUrl;
-	}
-	
-	private Header createBasicAuthenticationHeader(String username, String password) {
-		String credentials = String.format("%s:%s", username, password);
-		String encoded = Base64.encodeToString(credentials.getBytes(), Base64.NO_WRAP);
-		return new BasicHeader("Authorization", String.format("%s %s", "Basic", encoded));
-	}
-	
-	private AsyncHttpClient createHttpClient() {
-		AsyncHttpClient client = new AsyncHttpClient();
-		client.addHeader("platform-ver-build", appVersion);
-		return client;
-	}
-	
-	private AsyncHttpClient createAutheniticatedHttpClient(String username, String password) {
-		AsyncHttpClient client = createHttpClient();
-		Header header = createBasicAuthenticationHeader(username, password);
-		client.addHeader(header.getName(), header.getValue());
-		return client;
-	}
-	
-	private String getAppVersion() {
-		return prefs.getString("platform_ver_build", "");
-	}
+    public void get(String url, RequestParams params,
+            AsyncHttpResponseHandler responseHandler) {
+        RequestParams reqParams = prepareParams(params);
+        Log.d("rc", baseUrl);
+        client.get(getAbsoluteUrl(url), reqParams, responseHandler);
+    }
+
+    public void post(Context context, String url, int id, Model model,
+            AsyncHttpResponseHandler response)
+            throws UnsupportedEncodingException {
+        String completeUrl = getAbsoluteUrl(url) + id;
+        completeUrl = prepareUrl(completeUrl);
+        client.post(context, completeUrl, new StringEntity(model.getData()
+                .toString()), "application/json", response);
+    }
+
+    public void post(Context context, String url, Model model,
+            AsyncHttpResponseHandler response)
+            throws UnsupportedEncodingException {
+        String completeUrl = getAbsoluteUrl(url);
+        completeUrl = prepareUrl(completeUrl);
+        client.post(context, completeUrl, new StringEntity(model.getData()
+                .toString()), "application/json", response);
+    }
+
+    public void post(String url, RequestParams params,
+            AsyncHttpResponseHandler responseHandler) {
+        RequestParams reqParams = prepareParams(params);
+        client.post(getAbsoluteUrl(url), reqParams, responseHandler);
+    }
+
+    public void put(Context context, String url, int id, Model model,
+            AsyncHttpResponseHandler response)
+            throws UnsupportedEncodingException {
+        String completeUrl = getAbsoluteUrl(url) + id;
+        completeUrl = prepareUrl(completeUrl);
+        client.put(context, completeUrl, new StringEntity(model.getData()
+                .toString()), "application/json", response);
+    }
+
+    /**
+     * Executes a get request and adds basic authentication headers to the
+     * request.
+     */
+    public void getWithAuthentication(Context context, String url,
+            String username, String password, RequestParams params,
+            AsyncHttpResponseHandler responseHandler) {
+        RequestParams reqParams = prepareParams(params);
+        Header[] headers = { createBasicAuthenticationHeader(username, password) };
+        client.get(context, getAbsoluteUrl(url), headers, reqParams,
+                responseHandler);
+    }
+
+    /**
+     * Executes a put request and adds basic authentication headers to the
+     * request.
+     */
+    public void putWithAuthentication(Context context, String url,
+            String username, String password, int id, Model model,
+            AsyncHttpResponseHandler response)
+            throws UnsupportedEncodingException {
+
+        String completeUrl = getAbsoluteUrl(url) + id;
+        completeUrl = prepareUrl(completeUrl);
+        Header[] headers = { createBasicAuthenticationHeader(username, password) };
+        StringEntity modelEntity = new StringEntity(model.getData().toString());
+        Log.d("REST", model.getData().toString());
+        client.put(context, completeUrl, headers, modelEntity,
+                "application/json", response);
+    }
+
+    public void putWithAuthentication(Context context, String url,
+            String username, String password, Model model,
+            AsyncHttpResponseHandler response)
+            throws UnsupportedEncodingException {
+
+        String completeUrl = getAbsoluteUrl(url);
+        completeUrl = prepareUrl(completeUrl);
+        Header[] headers = { createBasicAuthenticationHeader(username, password) };
+        StringEntity modelEntity = new StringEntity(model.getData().toString());
+
+        client.put(context, completeUrl, headers, modelEntity,
+                "application/json", response);
+    }
+
+    /**
+     * Executes a post request and adds basic authentication headers to the
+     * request.
+     */
+    public void postWithAuthentication(Context context, String url,
+            String username, String password, Model model,
+            AsyncHttpResponseHandler responseHandler)
+            throws UnsupportedEncodingException {
+
+        String completeUrl = getAbsoluteUrl(url);
+        completeUrl = prepareUrl(completeUrl);
+        Header[] headers = { createBasicAuthenticationHeader(username, password) };
+        StringEntity modelEntity = new StringEntity(model.getData().toString());
+
+        Log.d("REST", model.getData().toString());
+        client.post(context, completeUrl, headers, modelEntity,
+                "application/json", responseHandler);
+    }
+
+    public void postWithAuthentication(Context context, String url,
+            String username, String password,
+            AsyncHttpResponseHandler responseHandler)
+            throws UnsupportedEncodingException {
+
+        String completeUrl = getAbsoluteUrl(url);
+        completeUrl = prepareUrl(completeUrl);
+        Header[] headers = { createBasicAuthenticationHeader(username, password) };
+        StringEntity modelEntity = new StringEntity("");
+        client.post(context, completeUrl, headers, modelEntity,
+                "application/json", responseHandler);
+    }
+
+    // This overloading of the postWithAuthentication method takes a bitmap, and
+    // posts
+    // it as an PNG HTTP Entity.
+    public void postWithAuthentication(Context context, String url, Bitmap bm,
+            String username, String password,
+            JsonHttpResponseHandler responseHandler, int timeout) {
+
+        String completeUrl = getAbsoluteUrl(url);
+        completeUrl = prepareUrl(completeUrl);
+
+        // Content type also needs to be pinned down in the Bitmap.compress
+        // call, which is why I haven't exposed it as a parameter.
+        String contentType = "image/jpeg";
+
+        // We need to coerce the bitmap into a ByteArrayEntity so that we can
+        // post it.
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        bm.compress(CompressFormat.JPEG, 55, bos);
+        byte[] bitmapdata = bos.toByteArray();
+        ByteArrayEntity bae = new ByteArrayEntity(bitmapdata);
+
+        // This client needs authentication headers because there is no post
+        // method that takes both
+        // an entity and headers. I assert that we don't get anything for free
+        // anyway by having one
+        // global AsyncHttpClient object because instantiation of that class is
+        // so easy.
+        AsyncHttpClient authenticatedClient = createAutheniticatedHttpClient(
+                username, password);
+
+        authenticatedClient.setTimeout(timeout);
+        authenticatedClient.post(context, completeUrl, bae, contentType,
+                responseHandler);
+    }
+
+    public void delete(String url, AsyncHttpResponseHandler responseHandler) {
+        client.delete(getAbsoluteUrl(url), responseHandler);
+    }
+
+    /**
+     * Executes a delete request and adds basic authentication headers to the
+     * request.
+     */
+    public void deleteWithAuthentication(Context context, String url,
+            String username, String password,
+            AsyncHttpResponseHandler responseHandler) {
+        String completeUrl = getAbsoluteUrl(url);
+        completeUrl = prepareUrl(completeUrl);
+        Header[] headers = { createBasicAuthenticationHeader(username, password) };
+        client.delete(context, completeUrl, headers, responseHandler);
+    }
+
+    private RequestParams prepareParams(RequestParams params) {
+        // We'll always need a RequestParams object since we'll always
+        // be sending an apikey
+        RequestParams reqParams;
+        if (params == null) {
+            reqParams = new RequestParams();
+        } else {
+            reqParams = params;
+        }
+
+        reqParams.put("apikey", apiKey);
+
+        return reqParams;
+    }
+
+    private String prepareUrl(String url) {
+        // Not all methods of AsynchHttpClients take a requestParams.
+        // Sometimes we will need to put the api key and other data
+        // directly in the URL.
+        return url + "?apikey=" + getApiKey();
+    }
+
+    private String getBaseUrl() {
+        String baseUrl = prefs.getString("base_url", "");
+        return baseUrl;
+    }
+
+    private String getApiKey() {
+        String apiKey = prefs.getString("api_key", "");
+        return apiKey;
+    }
+
+    private String getAbsoluteUrl(String relativeUrl) {
+        Log.d(App.LOG_TAG, baseUrl + relativeUrl);
+        return baseUrl + relativeUrl;
+    }
+
+    private Header createBasicAuthenticationHeader(String username,
+            String password) {
+        String credentials = String.format("%s:%s", username, password);
+        String encoded = Base64.encodeToString(credentials.getBytes(),
+                Base64.NO_WRAP);
+        return new BasicHeader("Authorization", String.format("%s %s", "Basic",
+                encoded));
+    }
+
+    private AsyncHttpClient createHttpClient() {
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.addHeader("platform-ver-build", appVersion);
+        return client;
+    }
+
+    private AsyncHttpClient createAutheniticatedHttpClient(String username,
+            String password) {
+        AsyncHttpClient client = createHttpClient();
+        Header header = createBasicAuthenticationHeader(username, password);
+        client.addHeader(header.getName(), header.getValue());
+        return client;
+    }
+
+    private String getAppVersion() {
+        return prefs.getString("platform_ver_build", "");
+    }
 }
