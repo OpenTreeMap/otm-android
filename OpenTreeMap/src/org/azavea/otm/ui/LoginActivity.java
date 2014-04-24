66077c6a2d050341bf5d5bd6c2ddffa61a3422bd
Michael Maurizi <mmaurizi@azavea.com>
2014-04-24 11:14:08 -0400

    Auto-formatting and whitespace cleanup

--- OpenTreeMap/src/org/azavea/otm/ui/LoginActivity.java
+++ OpenTreeMap/src/org/azavea/otm/ui/LoginActivity.java
@@ -16,65 +16,59 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 public class LoginActivity extends Activity {
-	private final LoginManager loginManager = App.getLoginManager();
+    private final LoginManager loginManager = App.getLoginManager();
 
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
         getWindow().setLayout(LayoutParams.FILL_PARENT, LayoutParams.FILL_PARENT);
     }
-    
+
     private boolean validate(String user, String pass) {
-    	if ("".equals(user) || "".equals(pass)) {
-    		String msg = "Please enter both a Username and a Password";
-    		Toast.makeText(this, msg, Toast.LENGTH_LONG).show();
-    		return false;
-    	}
-    	return true;
+        if ("".equals(user) || "".equals(pass)) {
+            String msg = "Please enter both a Username and a Password";
+            Toast.makeText(this, msg, Toast.LENGTH_LONG).show();
+            return false;
+        }
+        return true;
     }
-    
+
     public void cancel(View view) {
-    	setResult(RESULT_CANCELED);
-    	finish();
+        setResult(RESULT_CANCELED);
+        finish();
     }
-    
+
     public void login(View view) {
-    	String username = ((EditText)findViewById(R.id.login_username))
-    			.getText().toString().trim();
-    	String password = ((EditText)findViewById(R.id.login_password))
-    			.getText().toString().trim();
-    
-    	if (validate(username, password)) {
-    		requestLogin(username, password);
-    	}
+        String username = ((EditText) findViewById(R.id.login_username)).getText().toString().trim();
+        String password = ((EditText) findViewById(R.id.login_password)).getText().toString().trim();
+
+        if (validate(username, password)) {
+            requestLogin(username, password);
+        }
     }
 
-	private void requestLogin(String username, String password) {
-		final ProgressDialog dialog = ProgressDialog.show(this, "", 
-                "Logging in...", true, true);
-		loginManager.logIn(this, username, password, new Callback() {
+    private void requestLogin(String username, String password) {
+        final ProgressDialog dialog = ProgressDialog.show(this, "", "Logging in...", true, true);
+        loginManager.logIn(this, username, password, new Callback() {
 
-			@Override
-			public boolean handleMessage(Message msg) {
-				dialog.cancel();
-				Bundle data = msg.getData();
-				if(data.getBoolean("success")) {
-					setResult(RESULT_OK);
-					finish();
-		            return true;
-				} else {
-					Toast.makeText(App.getAppInstance(), 
-							data.getString("message"), 
-							Toast.LENGTH_LONG).show();
-					return false;
-				}
-			} 
-		});
-	} 
-	
-	public void register(View view) {
-		startActivity(new Intent(this, Register.class));
-	}
+            @Override
+            public boolean handleMessage(Message msg) {
+                dialog.cancel();
+                Bundle data = msg.getData();
+                if (data.getBoolean("success")) {
+                    setResult(RESULT_OK);
+                    finish();
+                    return true;
+                } else {
+                    Toast.makeText(App.getAppInstance(), data.getString("message"), Toast.LENGTH_LONG).show();
+                    return false;
+                }
+            }
+        });
+    }
+
+    public void register(View view) {
+        startActivity(new Intent(this, Register.class));
+    }
 }
