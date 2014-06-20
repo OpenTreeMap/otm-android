3eba74cb7b8a1ca32ec7ad01dd73ce5588c3773b
Michael Maurizi <mmaurizi@azavea.com>
2014-06-20 15:26:34 -0400

    Wait at least 2 seconds before leaving Splash Screen
    Use jDeferred for promises, in order to wait until *both* the AJAX request
    has finished *and* 2 seconds have elapsed
    
    Fixes #94

--- OpenTreeMap/src/org/azavea/otm/ui/SplashScreenActivity.java
+++ OpenTreeMap/src/org/azavea/otm/ui/SplashScreenActivity.java
@@ -3,6 +3,7 @@ package org.azavea.otm.ui;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.Handler.Callback;
 import android.os.Message;
 import android.webkit.WebView;
@@ -10,8 +11,22 @@ import android.webkit.WebView;
 import org.azavea.otm.App;
 import org.azavea.otm.R;
 import org.azavea.otm.rest.handlers.RestHandler;
+import org.jdeferred.Deferred;
+import org.jdeferred.DeferredFutureTask;
+import org.jdeferred.DeferredManager;
+import org.jdeferred.DeferredRunnable;
+import org.jdeferred.DoneCallback;
+import org.jdeferred.Promise;
+import org.jdeferred.android.AndroidDeferredManager;
+import org.jdeferred.android.AndroidDeferredObject;
+import org.jdeferred.impl.DeferredObject;
+import org.jdeferred.impl.DeferredPromise;
+
+import java.util.concurrent.Callable;
 
 public class SplashScreenActivity extends Activity {
+
+    private static final int SPLASH_SCREEN_DELAY_MILLIS = 2000;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -22,21 +37,37 @@ public class SplashScreenActivity extends Activity {
         WebView wv = (WebView) findViewById(R.id.splash_webview);
         wv.loadUrl("file:///android_asset/splash_content.html");
 
+        // The 2nd and 3rd type parameters are required but unused
+        // (They would be the arguments for promise.reject and promise.progress)
+        final Deferred<Bundle, Throwable, Integer> deferred = new DeferredObject<Bundle, Throwable, Integer>();
+        final Promise<Bundle, Throwable, Integer> promise = deferred.promise();
+
         App.getLoginManager().autoLogin(new Callback() {
             @Override
             public boolean handleMessage(final Message msg) {
-                final Bundle args = msg.getData();
-
-                // Only skip the instance switcher for a skinned app
-                // or if the login succeeded on a non-skinned app
-                if (args != null && args.getBoolean(RestHandler.SUCCESS_KEY)) {
-                    redirect(App.hasInstanceCode());
-                } else {
-                    redirect(App.hasSkinCode());
-                }
+                deferred.resolve(msg.getData());
                 return true;
             }
         });
+
+        Handler splashHandler = new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+                promise.done(new DoneCallback<Bundle>() {
+                    @Override
+                    public void onDone(Bundle args) {
+                        // Only skip the instance switcher for a skinned app
+                        // or if the login succeeded on a non-skinned app
+                        if (args != null && args.getBoolean(RestHandler.SUCCESS_KEY)) {
+                            redirect(App.hasInstanceCode());
+                        } else {
+                            redirect(App.hasSkinCode());
+                        }
+                    }
+                });
+            }
+        };
+        splashHandler.sendMessageDelayed(Message.obtain(), SPLASH_SCREEN_DELAY_MILLIS);
     }
 
     private void redirect(final boolean skipInstanceSwitcher) {
